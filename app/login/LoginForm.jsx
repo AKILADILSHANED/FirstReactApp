c5a6fcb28974cfd229d8968225d6e0e22267b50d
Akila Dilshan Edirisooriya <edirisooriyaakila@gmail.com>
2024-12-26 20:42:24 +0530

    Created Login Form

---
+++ app/login/LoginForm.jsx
@@ -0,0 +1,83 @@
+"use client";
+import React, { useState } from "react";
+
+export default function LoginForm({title}) {
+  const [email, setemail] = useState("");
+  const [pswrd, setpassword] = useState("");
+ 
+  const submitHandle = (e)=>{
+    e.preventDefault()
+    alert("You submitted the data..")
+  }
+
+  return (
+    <div className="flex bg-white rounded-2xl shadow-2xl dark:border">
+      <form onSubmit={submitHandle} className="pr-20 pl-10 py-10">
+        <div>
+          <h6 className=" text-3xl text-center font-bold text-blue-500">
+            {title}
+          </h6>
+        </div>
+
+        <div className="flex flex-col py-6">
+          <label htmlFor="email">Your email</label>
+
+          <input
+            className="bg-gray-50 rounded-md border-2 h-10 w-[400px] hover:border-blue-500"
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => {
+                setemail(e.target.value)
+            }}
+            placeholder="name@company.com"></input>
+        </div>
+
+        <div className="flex flex-col py-1">
+          <label htmlFor="password">Password</label>
+
+          <input
+            className="bg-gray-50 rounded-md border-2 h-10 w-[400px] hover:border-blue-500 "
+            type="password"
+            name="password"
+            id="password"
+            value={pswrd}
+            onChange={(e) => {
+                setpassword(e.target.value)
+            }}
+            placeholder="• • • • • • • • • • •"></input>
+        </div>
+
+        <div className="flex flex-row py-2">
+          <input name="remember" id="checkbox" type="checkbox"></input>
+          <label htmlFor="checkbox" className="px-2 text-md">
+            Remember me
+          </label>
+          <a
+            href="#"
+            className="items-end pl-32 text-blue-500 hover:underline text-md">
+            Forgot Password?
+          </a>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            name="signin"
+            id="signin"
+            className="border text-center bg-blue-500 w-full h-10 rounded-lg font-bold text-white hover:bg-blue-700">
+            Sign in
+          </button>
+        </div>
+
+        <div className="py-5 flex justify-center">
+          <label>
+            Don't have an account?
+            <a className="px-2 text-blue-500 hover:underline">Sign up here</a>
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ app/login/page.jsx
@@ -0,0 +1,10 @@
+import LoginForm from "./LoginForm";
+import loginform from "./LoginForm";
+
+export default function login() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full min-h-screen ">
+      <LoginForm title="Sign in to Evotech!"/>
+    </div>
+  );
+}
